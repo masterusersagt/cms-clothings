84ffbaa8d26853f995c22387380c0e016209d664
Christos Mazarakis <christos.mazarakis@avision-it.de>
2023-04-13 10:54:16 +0200

    navigatione ende

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,15 @@
 import { Fragment } from 'react';
-import {Outlet, Link } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
+import { ReactComponent as ChmLogo } from '../../assets/crown.svg'
+
+import './navigation.styles.scss';
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className='navigation' >
         <Link className='logo-container' to='/'>
-          <div>Logo</div>
+          <ChmLogo className='logo' />
         </Link>
         <div className='nav-links-container'>
           <Link className='nav-link' to='/shop'>SHOP</Link>
