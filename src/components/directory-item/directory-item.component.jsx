649c6673ac95f822cc000c1a6a89f607d31e0d48
Christos Mazarakis <christos.mazarakis@avision-it.de>
2023-04-18 14:13:28 +0200

    mehr styled component

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,23 +1,16 @@
-import './directory-item.styles.scss';
+import { BackgroundImage, DirectoryItemBody, DirectoryItemContainer } from './directory-item.styles';
 
 const DirectoryItem = ({ category }) => {
   const { imageUrl, title } = category;
 
   return (
-    <div className='directory-item-container'>
-      <div
-        className='background-image'
-        style={
-          {
-            backgroundImage: `url(${imageUrl})`,
-          }
-        }
-      />
-      <div className='directory-item-body'>
+    <DirectoryItemContainer>
+      <BackgroundImage imageUrl={imageUrl} />
+      <DirectoryItemBody>
         <h2>{title}</h2>
         <p>Shop Now</p>
-      </div>
-    </div>
+      </DirectoryItemBody>
+    </DirectoryItemContainer>
   );
 };
 
